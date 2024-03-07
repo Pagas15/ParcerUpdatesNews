6f6241f4e14695b5baf41c5c9a188c753d7001dd
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-03-07 22:31:58 +0000

    Added setup_window method in SiteObserver

--- bot/misc/observer.py
+++ bot/misc/observer.py
@@ -29,13 +29,15 @@ class SiteObserver(Observer):
     def __init__(self, chrom_webdriver: Chrome, site_snapshot: SiteSnapshot):
         self.chrome_driver = chrom_webdriver
         self.site_snapshot = site_snapshot
+        self.setup_window()
 
     def monitor(self, url: str, by: str, by_value: str, sleep_time: int) -> bool:
         validators.url(url)
         try:
             self.chrome_driver.get(url)
             time.sleep(sleep_time)
-            element = WebDriverWait(self.chrome_driver, self.loading_timeout).until(EC.presence_of_element_located((by, by_value)))
+            element = WebDriverWait(self.chrome_driver, self.loading_timeout).until(
+                EC.presence_of_element_located((by, by_value)))
             element_content = element.text
 
         except TimeoutException:
@@ -45,7 +47,8 @@ class SiteObserver(Observer):
         try:
             if self.site_snapshot.get_snapshot() != element_content:
                 self.site_snapshot.make_snapshot(element_content)
-                ActionChains(self.chrome_driver).move_to_element(self.chrome_driver.find_element(by, by_value)).perform()
+                ActionChains(self.chrome_driver).move_to_element(
+                    self.chrome_driver.find_element(by, by_value)).perform()
                 self.chrome_driver.execute_script("document.body.style.zoom='65%'")
                 element.screenshot(self.site_snapshot.screenshot_path)
                 return True
@@ -55,4 +58,11 @@ class SiteObserver(Observer):
         except FileNotFoundError:
             self.site_snapshot.make_snapshot(element_content)
 
+    def setup_window(self, required_width=None, required_height=None):
+        if required_width is None:
+            required_width = self.chrome_driver.execute_script('return document.body.parentNode.scrollWidth')
 
+        if required_height is None:
+            required_height = self.chrome_driver.execute_script('return document.body.parentNode.scrollHeight')
+
+        self.chrome_driver.set_window_size(required_width, required_height)
